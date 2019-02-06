5fc658bc9b8998d56b71aea7ea1f9bbe5c4bdebd
rajubd <raju.rayhan@yandex.com>
2019-02-06 14:22:09 +0600

    UI Update on Progress

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Funbox</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css"> -->
+  <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+
+  <!-- Plugin CSS -->
+  <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/freelancer.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="img-responsive" src="/funboxui/img/funbox.png"></a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase bg-funbox text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Games</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Video</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Wallpaper</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Animation</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Music</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <!-- <header class="masthead bg-primary text-white text-center">
+    <div class="container">
+      <img class="img-fluid mb-5 d-block mx-auto" src="img/profile.png" alt="">
+      <h1 class="text-uppercase mb-0">Start Bootstrap</h1>
+      <hr class="star-light">
+      <h2 class="font-weight-light mb-0">Web Developer - Graphic Artist - User Experience Designer</h2>
+    </div>
+  </header> -->
+
+  <!-- Slider  -->
+
+  <section id="slider">
+    <div class="container">
+      <div id="demo" class="carousel slide" data-ride="carousel">
+
+        <!-- Indicators -->
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        
+        <!-- The slideshow -->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="http://www.funboxbd.com/club/assets/img/funbox-carousel-01.jpg" alt="Los Angeles" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="http://www.funboxbd.com/club/assets/img/funbox-carousel-08.jpg" alt="Chicago" width="1100" height="500">
+          </div>
+          <div class="carousel-item">
+            <img src="http://www.funboxbd.com/club/assets/img/funbox-carousel-13.jpg" alt="New York" width="1100" height="500">
+          </div>
+        </div>
+        
+        <!-- Left and right controls -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- Buttons -->
+  <div class="container" id="buttons">
+    <div class="row">
+      <div class="col-lg-12">
+          <button class="btn btn-success" id="subscribeBtn">Subscribe</button> 
+          <button class="btn btn-success" id="myAccountBtn">My Account</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Games -->
+  <div class="container contentBlock" id="games">
+      <div class="row banner">
+      <a class="col-xs-12 col-sm-12 column">
+          <div class="gameCol">
+            <h1 class="text-center text-uppercase text-secondary mb-0 pb-20 pt-20">Games</h1>
+          </div>
+      </a>
+      </div>
+  </div>
+
+  <!-- Video -->
+  <div class="container contentBlock" id="video">
+      <div class="row banner">
+        <a class="col-xs-12 col-sm-12 column">
+            <img class="img-responsive" src="/funboxui/img/banner.gif">
+        </a>
+      </div>
+  </div>
+
+  <!-- Wallpaper -->
+  <div class="container contentBlock" id="wallpaper">
+      <div class="row banner">
+        <a class="col-xs-12 col-sm-12 column">
+            <img class="img-responsive" src="/funboxui/img/banner.gif">
+        </a>
+      </div>
+  </div>
+
+  <!-- Animation -->
+  <div class="container contentBlock" id="animation">
+      <div class="row banner">
+        <a class="col-xs-12 col-sm-12 column">
+            <img class="img-responsive" src="/funboxui/img/banner.gif">
+        </a>
+      </div>
+  </div>
+
+  <!-- MP3 -->
+  <div class="container contentBlock" id="mp3">
+      <div class="row banner">
+        <a class="col-xs-12 col-sm-12 column">
+            <img class="img-responsive" src="/funboxui/img/banner.gif">
+        </a>
+      </div>
+  </div>
+
+  <!-- Footer -->
+  <footer class="footer text-justify">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>
+          Download 5 free attractive contents everyday by subscribing to Funbox service only @ BDT 2.44 including (VAT+SD+SC) per day. To subscribe please click the subscribe button. Thank You!
+          </p>
+          <p class="text-center">
+            For Help Call <br> 01977733255, 01636007777, 01860744898
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <div class="copyright py-4 text-center text-white">
+    <div class="container">
+      <small>&copy; FunBox <?php echo date('Y'); ?></small>
+    </div>
+  </div>
+
+  <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+  <div class="scroll-to-top d-lg-none position-fixed ">
+    <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
+      <i class="fa fa-chevron-up"></i>
+    </a>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/freelancer.min.js"></script>
+
+</body>
+
+</html>

b7cec603c51328d4a71557eb407e814c374102ab
rajubd <raju.rayhan@yandex.com>
2019-02-06 17:17:47 +0600

    Navbar on Click Close Done.

--- index.php
+++ index.php
@@ -215,4 +215,16 @@
 
 </body>
 
+<script>
+   $(document).ready(function () {
+      $(document).click(function (event) {
+          var clickover = $(event.target);
+          var _opened = $(".navbar-collapse").hasClass("show");
+          if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+              $(".navbar-toggler").click();
+          }
+      });
+  });
+</script>
+
 </html>

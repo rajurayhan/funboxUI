f3cf058058c708893f7cb1bd0a86c23a3c62af99
rajubd <raju.rayhan@yandex.com>
2019-02-14 13:19:18 +0600

    Navigation Segmented, Made changes suggested by Sweety

--- index.php
+++ index.php
@@ -31,35 +31,7 @@
 
 <body id="page-top">
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class="img-responsive" src="/funboxui/img/funbox.png"></a>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase bg-funbox text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/funboxui/games.php">Games</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Video</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Wallpaper</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Animation</a>
-          </li>
-          <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Music</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?php include './includes/nav.php';?>
 
   <!-- Header -->
   <!-- <header class="masthead bg-primary text-white text-center">
